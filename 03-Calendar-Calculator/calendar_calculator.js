d40cd25d935eb6d206cb9570cdee9cec4caf02a1
fithawi ghide <lgfetah@gmail.com>
2021-02-19 12:24:35 +0100

    commit with doing nothing today

--- 03-Calendar-Calculator/calendar_calculator.js
+++ 03-Calendar-Calculator/calendar_calculator.js
@@ -4,3 +4,16 @@ here
 */
 
 //Insert your code below
+var month, X, listOfMonth, day, valueOfMonth, indexOfMonth;
+
+
+month = window.prompt('Enter the month');
+day = Number(window.prompt('Enter the date'));
+
+listOfMonth = 'Jan,Feb,Mar'.split(',');
+valueOfMonth = '0,31,59'.split(',');
+indexOfMonth = listOfMonth.indexOf(month) + 1;
+
+X = valueOfMonth[(indexOfMonth - 1)];
+
+console.log((X - 0) + day);
--- 04-Password-Generator/password_generator.js
+++ 04-Password-Generator/password_generator.js
@@ -4,3 +4,13 @@ here
 */
 
 //insert your code below
+function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+generatePassword();
