24dea40b7226fdc02065cf9fe7047bec93ef9428
fithawi ghide <lgfetah@gmail.com>
2021-02-20 19:42:22 +0100

    working generatore password randomly source from stackOver flow

--- 04-Password-Generator/password_generator.js
+++ 04-Password-Generator/password_generator.js
@@ -1,16 +1,22 @@
 //insert your pseudocode below
-/*
-here
+/*The programe should print random password.
+*create a variable to store the length of the password 
+*create a variable to store the char 
+*create an empty string  variable to store the random password
+*by looping over the charset as length of the password
+*then return value
+
+
 */
 
 //insert your code below
 function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    var length = 20,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]{}*+#,;?=()&%$£",
         retVal = "";
     for (var i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
     return retVal;
 }
-generatePassword();
+console.log(generatePassword());
